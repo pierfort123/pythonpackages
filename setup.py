490b178d1e1a25a5204d7c9fc1081e8a2b5bf2c7
Alex Clark <aclark@aclark.net>
2013-01-02 18:35:37 -0500

    Make it work

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@ from setuptools import setup
 setup(
     name='pythonpackages',
     packages=find_packages(),
+    entry_points={
+        'paste.app_factory': 'main=pythonpackages:main',
+    },
 )
