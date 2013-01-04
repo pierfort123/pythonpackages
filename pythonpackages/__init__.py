3af003af465d47501c70042bb324dc4aacdc385b
Alex Clark <aclark@aclark.net>
2013-01-04 18:11:26 -0500

    Hello world

--- pythonpackages/__init__.py
+++ pythonpackages/__init__.py
@@ -1,8 +1,17 @@
 from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def root(request):
+    """
+    """
+    return Response('Hello world')
 
 
 def main(global_config, **settings):
     """
     """
     config = Configurator()
+    config.add_route('root', '/')
+    config.add_view(root, route_name='root')
     return config.make_wsgi_app()
