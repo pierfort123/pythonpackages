7ceefc54c3a1ec3818c78b457307a2318623eab1
Alex Clark <aclark@aclark.net>
2013-01-04 18:47:20 -0500

    Add renderer

--- pythonpackages/__init__.py
+++ pythonpackages/__init__.py
@@ -1,5 +1,4 @@
 from pyramid.config import Configurator
-from pyramid.response import Response
 
 
 def root(request):
@@ -13,5 +12,6 @@ def main(global_config, **settings):
     """
     config = Configurator()
     config.add_route('root', '/')
-    config.add_view(root, route_name='root')
+    config.add_view(root, route_name='root',
+         renderer='pythonpackages:templates/root.mak')
     return config.make_wsgi_app()
