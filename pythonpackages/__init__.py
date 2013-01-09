d003d4284ee0ec3bd86bec0b8e6610a5a47d4b74
Alex Clark <aclark@aclark.net>
2013-01-08 19:56:58 -0500

    Add login view, route, template

--- pythonpackages/__init__.py
+++ pythonpackages/__init__.py
@@ -11,7 +11,13 @@ def main(global_config, **settings):
     """
     """
     config = Configurator()
+
     config.add_route('root', '/')
     config.add_view(root, route_name='root',
          renderer='pythonpackages:templates/root.mak')
+
+    config.add_route('login', '/login')
+    config.add_view('pythonpackages.views.login', route_name='login',
+         renderer='pythonpackages:templates/login.mak')
+
     return config.make_wsgi_app()
