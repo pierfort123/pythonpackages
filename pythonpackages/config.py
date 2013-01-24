8b83247a734767d6ca2634ab1526fee3edadb47a
Alex Clark <aclark@aclark.net>
2013-01-24 07:48:18 -0500

    Fix config vars

--- pythonpackages/config.py
+++ pythonpackages/config.py
@@ -1,8 +1,8 @@
 import os
 
 
-GITHUB_CLIENT_ID = os.environ.get(GITHUB_CLIENT_ID, None)
-GITHUB_CLIENT_SECRET = os.environ.get(GITHUB_CLIENT_SECRET, None)
+GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID', None)
+GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET', None)
 GITHUB_SCOPE = ''  # http://developer.github.com/v3/oauth/#scopes
 GITHUB_URL = 'https://github.com'
 GITHUB_URL_API = 'https://api.github.com'
@@ -13,5 +13,5 @@ GITHUB_URL_AUTH = (GITHUB_URL + '/login/oauth/authorize?client_id=%s&scope=%s'
 #REDIS_KEY_USER_PYPI_OAUTH_SECRET
 #REDIS_KEY_USER_PYPI_OAUTH_SECRET
 #REDIS_KEY_USER_PYPI_OAUTH_TOKEN
-PYPI_OAUTH_CONSUMER_KEY = os.environ.get(PYPI_OAUTH_CONSUMER_KEY, None)
-PYPI_OAUTH_CONSUMER_SECRET = os.environ.get(PYPI_OAUTH_CONSUMER_SECRET, None)
+PYPI_OAUTH_CONSUMER_KEY = os.environ.get('PYPI_OAUTH_CONSUMER_KEY', None)
+PYPI_OAUTH_CONSUMER_SECRET = os.environ.get('PYPI_OAUTH_CONSUMER_SECRET', None)
