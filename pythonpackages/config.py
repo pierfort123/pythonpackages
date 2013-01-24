caf5cf6782426a13bc8bb98189ad8a33e01402d0
Alex Clark <aclark@aclark.net>
2013-01-24 08:24:59 -0500

    Add redis config

--- pythonpackages/config.py
+++ pythonpackages/config.py
@@ -15,3 +15,11 @@ GITHUB_URL_AUTH = (GITHUB_URL + '/login/oauth/authorize?client_id=%s&scope=%s'
 #REDIS_KEY_USER_PYPI_OAUTH_TOKEN
 PYPI_OAUTH_CONSUMER_KEY = os.environ.get('PYPI_OAUTH_CONSUMER_KEY', None)
 PYPI_OAUTH_CONSUMER_SECRET = os.environ.get('PYPI_OAUTH_CONSUMER_SECRET', None)
+REDIS = None
+if 'REDISTOGO_URL' in os.environ:  # XXX Replace with Redis Cloud config
+    urlparse.uses_netloc.append('redis')
+    url = urlparse.urlparse(os.environ['REDISTOGO_URL'])
+    REDIS = redis.Redis(host=url.hostname, port=url.port, db=0,
+        password=url.password)
+else:
+    REDIS = redis.StrictRedis(host='localhost', port=6379, db=0)
