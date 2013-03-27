03c6d3bcb392900544921a6813e3aadb2f5b10a0
Alex Clark <aclark@aclark.net>
2013-03-27 08:09:04 -0400

    Add about view

--- pythonpackages/__init__.py
+++ pythonpackages/__init__.py
@@ -16,6 +16,11 @@ def main(global_config, **settings):
     config.add_view(root, route_name='root',
          renderer='pythonpackages:templates/root.mak')
 
+    config.add_route('about', '/about')
+    config.add_view('pythonpackages.views.about',
+        route_name='about',
+        renderer='pythonpackages:templates/about.mak')
+
     config.add_route('login', '/login')
     config.add_view('pythonpackages.views.login', route_name='login',
          renderer='pythonpackages:templates/login.mak')
