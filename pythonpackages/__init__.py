5daa42b5186c6aecaf7210ea73ebcd49f28f72f1
Alex Clark <aclark@aclark.net>
2013-10-14 15:14:07 -0400

    Fix errors ; clean up

--- pythonpackages/__init__.py
+++ pythonpackages/__init__.py
@@ -23,23 +23,11 @@ def main(global_config, **settings):
         route_name='root',
         renderer='pythonpackages:templates/root.mak')
 
-    config.add_route('about', '/about')
+    config.add_route('logout', '/logout')
     config.add_view(
-        'pythonpackages.views.about',
-        route_name='about',
-        renderer='pythonpackages:templates/about.mak')
-
-    config.add_route('contact', '/contact')
-    config.add_view(
-        'pythonpackages.views.contact',
-        route_name='contact',
-        renderer='pythonpackages:templates/contact.mak')
-
-    config.add_route('login', '/login')
-    config.add_view(
-        'pythonpackages.views.login',
-        route_name='login',
-        renderer='pythonpackages:templates/login.mak')
+        'pythonpackages.views.logout',
+        route_name='logout',
+        renderer='pythonpackages:templates/logout.mak')
 
     config.add_static_view(
         'static', 'pythonpackages:static', cache_max_age=3600)
