d74026a2373c25ca7d7c2390d0e9c73a9694c7f4
Alex Clark <aclark@aclark.net>
2013-10-14 20:14:20 -0400

    Debug

--- pythonpackages/views.py
+++ pythonpackages/views.py
@@ -49,6 +49,6 @@ def root(request):
         headers = remember(request, user)
 #        return HTTPFound(location="/", headers=headers)
     return {
-        'user_info': code,
+        'user_info': user_info,
         'auth_url': GH_LOGIN_AUTH,
     }
