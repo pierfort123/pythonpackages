70bdeda7c1dd845fca1767c1cfc4021fe5868412
Alex Clark <aclark@aclark.net>
2013-10-15 18:06:21 -0400

    Add test(s)

--- pythonpackages/tests.py
+++ pythonpackages/tests.py
@@ -1,13 +1,24 @@
-import unittest
 from pyramid import testing
+import unittest
 
-class TestPythonPackages(unittest.TestCase):
+
+class ViewIntegrationTests(unittest.TestCase):
     def setUp(self):
-        request = testing.DummyRequest()
-        self.config = testing.setUp(request=request)
+        """
+        """
+        self.config = testing.setUp()
 
     def tearDown(self):
+        """ Clear out the application registry """
         testing.tearDown()
 
-    def test_something(self):
-        assert True
+    def test_root(self):
+        from pythonpackages.views import root
+        request = testing.DummyRequest()
+        result = root(request)
+        self.assertEqual(request.response.status, '200 OK')
+        self.assertTrue('auth_url' in result)
+        self.assertTrue('logged_in' in result)
+        self.assertTrue('user' in result)
+        self.assertEqual(request.response.headerlist[0],
+                         ('Content-Type', 'text/html; charset=UTF-8'))
