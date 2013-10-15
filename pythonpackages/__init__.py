7b313d20784fc013ee8bd312da71f6f8598541f6
Alex Clark <aclark@aclark.net>
2013-10-14 21:32:18 -0400

    Fix redis sessions ; add auth and auth policies

--- pythonpackages/__init__.py
+++ pythonpackages/__init__.py
@@ -1,7 +1,11 @@
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.config import Configurator
+from pyramid_redis_sessions import session_factory_from_settings
 import os
 import redis
 
+auth_secret = os.getenv('AUTH_POLICY_SECRET', '')
 redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 redis_secret = os.getenv('REDIS_SESSIONS_SECRET', '')
 redis = redis.from_url(redis_url)
@@ -10,11 +14,17 @@ redis = redis.from_url(redis_url)
 def main(global_config, **settings):
     """
     """
-
+    authentication_policy = AuthTktAuthenticationPolicy(auth_secret)
+    authorization_policy = ACLAuthorizationPolicy()
     settings['redis.sessions.secret'] = redis_secret
     settings['redis.sessions.url'] = redis_url
-    config = Configurator(settings=settings)
-
+    session_factory = session_factory_from_settings(settings)
+    config = Configurator(
+        authentication_policy=authentication_policy,
+        authorization_policy=authorization_policy,
+        session_factory=session_factory
+        settings=settings,
+        )
     config.add_route('root', '/')
     config.add_view(
         'pythonpackages.views.root',
