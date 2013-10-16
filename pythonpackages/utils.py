e7691512899b6338a47a293d1b124bfc612235f6
Alex Clark <aclark@aclark.net>
2013-10-16 06:44:19 -0400

    Add comment

--- pythonpackages/utils.py
+++ pythonpackages/utils.py
@@ -2,5 +2,9 @@ USER_LINK = "<a href='/%s'>%s</a> %s"
 
 
 def link_user(logged_in_entry):
+    """
+    Takes a string e.g. `aclark4life logged in 10/13/13` and returns
+    `<a href="/aclark4life">aclark4life</a> logged in 10/13/13`
+    """
     parts = logged_in_entry.split()
     return USER_LINK % (parts[0], parts[0], ' '.join([parts[1], parts[2], parts[3]]))
