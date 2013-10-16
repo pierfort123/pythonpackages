05aa3ff17aaea8dfd16292787ec76e66dca2500a
Alex Clark <aclark@aclark.net>
2013-10-16 19:41:11 -0400

    Fix errors

--- pythonpackages/views.py
+++ pythonpackages/views.py
@@ -108,7 +108,7 @@ def user(request):
     """
     """
     user = request.path_qs.strip('/')
-    if get_user(user)
+    if get_user(user):
         response['access_token'] = PYPI_TOKEN_URL
         response['user'] = user
         return response
