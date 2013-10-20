b8253934d29822cd83a0c126717aa06c7ca39165
Alex Clark <aclark@aclark.net>
2013-10-20 05:57:39 -0400

    Clean up

--- pythonpackages/config.py
+++ pythonpackages/config.py
@@ -1,6 +1,5 @@
 import os
 
-user_url = 'https://api.github.com/user?%s'
 
 _now = '%m/%d/%y'
 
@@ -11,5 +10,6 @@ auth_url = 'https://github.com/login/oauth/authorize?client_id=%s' % (
     client_id)
 
 token_url_gh = 'https://github.com/login/oauth/access_token'
-
 token_url_pypi = 'https://pypi.python.org/oauth/access_token'
+
+user_url = 'https://api.github.com/user?%s'
