de5b7348a8c3ea1e3e24f7578e8d38beb02e4542
Alex Clark <aclark@aclark.net>
2013-10-20 05:43:36 -0400

    Lowercase

--- pythonpackages/config.py
+++ pythonpackages/config.py
@@ -7,7 +7,7 @@ FORMAT = '%m/%d/%y'
 GH_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID', '')
 GH_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET', '')
 
-GH_AUTH_URL = 'https://github.com/login/oauth/authorize?client_id=%s' % (
+auth_url = 'https://github.com/login/oauth/authorize?client_id=%s' % (
     GH_CLIENT_ID)
 
 GH_TOKEN_URL = 'https://github.com/login/oauth/access_token'
--- pythonpackages/views.py
+++ pythonpackages/views.py
@@ -4,11 +4,11 @@ from pyramid.security import authenticated_userid
 from pyramid.security import forget
 from pyramid.security import has_permission
 from pyramid.security import remember
+from .config import auth_url
 from .config import API_GH_USER
 from .config import FORMAT
 from .config import GH_CLIENT_ID
 from .config import GH_CLIENT_SECRET
-from .config import GH_AUTH_URL
 from .config import GH_TOKEN_URL
 from .config import PYPI_TOKEN_URL
 from .db import db
@@ -27,7 +27,7 @@ def about(request):
     """
     userid = authenticated_userid(request)
     return {
-        'auth_url': GH_AUTH_URL,
+        'auth_url': auth_url,
         'user': userid,
     }
 
@@ -38,7 +38,7 @@ def activity(request):
     userid = authenticated_userid(request)
     logged_in = db.lrange('logged_in', 0, -1)
     return {
-        'auth_url': GH_AUTH_URL,
+        'auth_url': auth_url,
         'link_user': link_user,
         'logged_in': logged_in,
         'user': userid,
@@ -101,7 +101,7 @@ def root(request):
     userid = authenticated_userid(request)
     logged_in = db.lrange('logged_in', 0, 4)
     return {
-        'auth_url': GH_AUTH_URL,
+        'auth_url': auth_url,
         'link_user': link_user,
         'logged_in': logged_in,
         'user': userid,
@@ -116,7 +116,7 @@ def user(request):
         userid = authenticated_userid(request)
         return {
             'access_token': PYPI_TOKEN_URL,
-            'auth_url': GH_AUTH_URL,
+            'auth_url': auth_url,
             'has_permission': has_permission,
             'request': request,
             'path': path,
